fb52b83c7a17121a1ac36e06ea3901ea3f8ab81f
Novar <xuwab77@gmail.com>
2024-10-21 18:08:05 +0800

    feature: finish android config

--- src/android/CordovaBugly.java
+++ src/android/CordovaBugly.java
@@ -1,32 +1,90 @@
-package cordova-plugin-novar-bugly;
+package com.novar.plugin.bugly;
 
 import org.apache.cordova.CordovaPlugin;
+
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.ConfigXmlParser;
+import org.apache.cordova.CordovaPreferences;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.widget.Toast;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.os.Environment;
+import com.tencent.bugly.crashreport.CrashReport;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class CordovaBugly extends CordovaPlugin {
+
+    private static final String TAG = "CordovaBugly";
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        System.out.println("Bugly running.......");
+        initBugly();
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
+        Log.d(TAG, "execute called with action: " + action);
+        Toast.makeText(cordova.getContext(), "hulala------", Toast.LENGTH_LONG).show();
+        if (action.equals("sayHello")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.sayHello(message, callbackContext);
+            return true;
+        }
+
+        if (action.equals("testCrash")) {
+            this.testCrash();
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
+    private void sayHello(String message, CallbackContext callbackContext) {
+
         if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+            Toast.makeText(cordova.getActivity(), message, Toast.LENGTH_LONG).show();
+            Log.d(TAG, "Immediate toast shown" + message);
+            callbackContext.success("Native received: " + message);
         } else {
-            callbackContext.error("Expected one non-empty string argument.");
+            callbackContext.error("message is null");
         }
+    }
+
+    private void initBugly() {
+        Toast.makeText(cordova.getContext(), "initBugly------", Toast.LENGTH_LONG).show();
+        ConfigXmlParser parser = new ConfigXmlParser();
+        parser.parse(cordova.getActivity());// 解析res/xml/config.xml文件
+        Log.d("buglyPlugin", "try get appid");
+        CordovaPreferences preferences = parser.getPreferences();
+        String appId = preferences.getString("buglyAndroidAppId", "");
+        Log.d("buglyPlugin", "appid:" + appId);
+        Toast.makeText(cordova.getContext(), "initBugly------" + appId, Toast.LENGTH_LONG).show();
+        if (appId != null && appId.length() > 0) {
+            CrashReport.initCrashReport(cordova.getActivity().getApplicationContext(), appId, false); 
+        } else {
+            Log.e(TAG, "appId is null");
+        }
+    }
+
+    private void testCrash() {
+        Toast.makeText(cordova.getContext(), "testCrash------", Toast.LENGTH_LONG).show();
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                throw new RuntimeException("This is a crash");
+            }
+        });
     }
 }
--- www/CordovaBugly.js
+++ www/CordovaBugly.js
@@ -1,5 +1,41 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'CordovaBugly', 'coolMethod', [arg0]);
+var CordovaBugly = function () { };
+
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'CordovaBugly', 'coolMethod', [arg0]);
+// };
+
+// CordovaBugly.prototype.sayHello = function (arg0, success, error) {
+//     console.log('sayHello from plugin.js :', arg0);
+//     exec(success, error, 'CordovaBugly', 'sayHello', [arg0]);
+// };
+
+CordovaBugly.prototype.sayHello = function (arg0, success, error) {
+    
+    exec(function(result) {
+        console.log('sayHello exec success:', result);
+        success(result);
+    }, function(err) {
+        console.log('sayHello exec error:', err);
+        error(err);
+    }, 'CordovaBugly', 'sayHello', [arg0]);
+    
+    console.log('sayHello from plugin.js - end');
 };
+
+CordovaBugly.prototype.testCrash = function () {
+    exec(null, null, 'CordovaBugly', 'testCrash', []);
+};
+
+
+CordovaBugly.install = function () {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+    window.plugins.CordovaBugly = new CordovaBugly();
+    return window.plugins.CordovaBugly;
+};
+
+
+cordova.addConstructor(CordovaBugly.install);
